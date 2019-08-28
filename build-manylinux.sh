e7fc5e84b812c7da9be11d08e8adfb75281dcf49
Spencer Callicott <tuxprogrammer@gmail.com>
2019-08-28 13:32:53 -0500

    Auto build wheel files.

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #
 
-docker run --rm -e PLAT=manylinux1_x86_64 -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
-ls wheelhouse/
-docker run --rm -e PLAT=manylinux1_i686 -v `pwd`:/io quay.io/pypa/manylinux1_i686 linux32 /io/build-wheels.sh
-ls wheelhouse/
-docker run --rm -e PLAT=manylinux2010_x86_64 -v `pwd`:/io quay.io/pypa/manylinux2010_x86_64 /io/build-wheels.sh
-ls wheelhouse/
+docker run -e PLAT=manylinux2010_x86_64 -v /home/callicott/work/pandas:/io quay.io/pypa/manylinux2010_x86_64 /io/build-wheels.sh
--- build-wheels.sh
+++ build-wheels.sh
@@ -3,14 +3,22 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/requirements-dev.txt
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt &
 done
+wait
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/ &
+done
+wait
+
+ls wheelhouse
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
+
+ls /io/wheelhouse
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
